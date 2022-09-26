fce6c79b32cc51a25f15bef2506799b9429e060d
Elie Lévy <elie.levy.3@gmail.com>
2022-09-26 16:48:05 +0200

    cleaning code + removing format + adding output file for wrong answers + refacto

--- Evaluation_test.py
+++ Evaluation_test.py
@@ -1,32 +1,64 @@
 import pandas as pd
 from random import sample
+from contextlib import redirect_stdout
 
+def print_question(question, data): 
+    print("____________________________\n")
+    print(data["Question"][question]) 
+    print("____________________________\n")
+    for x in ["A","B","C","D","E"]:
+        if(str(data[x][question])!="nan"):
+            print(f"{x} : {data[x][question]}")
+
+def export_wrongly_anwsered_questions(questions, data): 
+    with open ('wrongly_answered_questions.txt', 'w') as out_file:
+        with redirect_stdout(out_file):
+            for question in questions: 
+                print_question(question, data)
+                print(f"\nThe right answer is : {data['result'][question]}")
 
 def main(n):
-    data = pd.read_csv("Evaluation_test.csv",sep=";") #Récupère les données
-    score =0 #Le score final obtenu
-    number =0 #le numéro de la question
+    # retrieve data 
+    data = pd.read_csv("Evaluation_test.csv",sep=";")
 
+    # Final Score obtained
+    score = 0
+
+    # question number 
+    number = 0 
+    
+    # list of questions sampled to avoid duplicate
     questions = sample(range(1, len(data)+1), n)
 
+    # list of wrongly answered questions that shall be displayed in a output file at the end of the exam
+    wrongly_answered_questions = []
     for i, number in enumerate(questions):
- 
-        print("\nQuestion {}".format(i+1)," sur {}:".format(n))
-        print("____________________________")
-        print(data["Question"][number]) #Affiche la question
-        print("____________________________")
-        for x in ["A","B","C","D","E"]:
-            if(str(data[x][number])!="nan"):
-                print("{}.".format(x)," {}".format(data[x][number])) #Affiche les réponses
-        reponse = input("\nEntrez votre réponse: ")
+
+        # Display questions and possible answers
+        print(f"\nQuestion {i} out of {n}")
+        print_question(number, data)
+
+        # blocking instruction that waits upon the user to enter an answer
+        reponse = input("\nEnter your anser : ")
+
+        # Checking results 
         if (reponse == data["result"][number]):
-            print("\nCORRECT. \n{} ".format(data["explanation"][number]))
-            score+=1
+            print("\nCORRECT\n")
+            score += 1
         else:
-            print("\nINCORRECT. \nla bonne réponse est {}".format(data["result"][number]),"\n{}".format(data["explanation"][number]))
+            print("\nWRONG\n")
+            print(f"The correct answer is : {data['result'][number]}")
+            wrongly_answered_questions.append(number)
+        print(f"Explanation : \n {data['explanation'][number]}")
+
+        # get wrongly answered questions with correct answers to an output file
+        export_wrongly_anwsered_questions(wrongly_answered_questions, data)
+
+    print("\nEnd of the exam !\n")
+    print(f"Score: {score} / {n}")
+    print(f"Accuracy percentage : {round(score / n, 3)} %")
 
 
-    print("\nFin de série !\nScore: {}".format(score),"/ {}".format(n))
 if __name__ == '__main__':
-    n = int(input("Combien de question souhaitez-vous ?: "))
+    n = int(input("How many questions do you want ? : "))
     main(n)
